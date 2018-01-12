32efabbe6fcc614c6fc1e543e51281af5b269993
Janos Potecki <janospotecki@Janoss-MacBook.local>
2018-01-12 20:17:55 +0000

    Init commit

---
+++ awslambdacontinuousdelivery/source/codecommit/__init__.py
@@ -0,0 +1,41 @@
+# By Janos Potecki
+# University College London
+# January 2018
+
+from troposphere import Template, Sub, Parameter, Ref
+from troposphere.codepipeline import ( Stages
+                                     , Actions
+                                     , ActionTypeID
+                                     , OutputArtifacts
+                                     )
+
+
+def getCodeCommit(t: Template, outputfiles: str) -> Stages:
+  repo = t.add_parameter(
+    Parameter( "CodeCommitRepo"
+             , Description="Name of the CodeCommit Repository"
+             , Type="String"
+             ) 
+    )
+  branch = t.add_parameter(
+    Parameter( "Branch"
+             , Description="Branch triggering the deployment"
+             , Type="String"
+             ) 
+    )
+  actionId = ActionTypeID( Category = "Source"
+                         , Owner = "AWS"
+                         , Version = "1"
+                         , Provider = "CodeCommit"
+                         )
+  action = Actions( Name = Sub("${AWS::StackName}-LambdaSource")
+                  , ActionTypeId = actionId
+                  , Configuration = {"BranchName" : Ref(branch)
+                                    , "RepositoryName" : Ref(repo)
+                                    }
+                  , OutputArtifacts = [OutputArtifacts( Name = outputfiles)]
+                  , RunOrder = "1"
+                  )
+  return Stages( Name = "Source"
+               , Actions = [ action ]
+               )
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name='AwsLambdaContinuousDeliverySourceCodeCommit'
+     , version = '0.0.1'
+     , description = 'AwsLambdaContinuousDeliverySourceCodeCommit'
+     , author = 'Janos Potecki'
+     , url = 'https://github.com/AwsLambdaContinuousDelivery/AwsLambdaContinuousDeliverySourceCodeCommit'
+     , packages = ['awslambdacontinuousdelivery.source.codecommit']
+     , license='MIT'
+     , install_requires = [ 
+          'troposphere'
+        ]
+     )
